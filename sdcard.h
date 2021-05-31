666a5d3b5022d4fa90efd6b21c0c49f5b92887c5
FilipeCoelhoAlves <filipeemmanoel@gmail.com>
2021-05-31 12:26:09 -0300

    Novo main

---
+++ sdcard.h
@@ -0,0 +1,12 @@
+#ifndef SDCARD_H_INCLUDED
+#define SDCARD_H_INCLUDED
+
+
+#define   DRV_MMC    0
+#include "ff14b/source/ff.h"     //Fat by ELM CHAN
+#include "ff14b/source/diskio.h" //Disk Functions
+
+
+
+#endif // SDCARD_H_INCLUDED
+
